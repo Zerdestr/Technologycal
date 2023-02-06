938dfaad00ff0e174234f5ad5f18bc1b12d9a54e
Zerdestr <ajiekc1004@gmail.com>
2023-02-06 17:01:28 +0200

    Project completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,3 @@
-// import * as flsFunctions from "./modules/functions.js";
-
-// flsFunctions.isWebp();
-
 import Swiper, { Navigation } from 'swiper';
 
 const swiperFamily = new Swiper('.swiper-family', {
@@ -44,6 +40,26 @@ const swiperFeatures = new Swiper('.features-family__swiper', {
   },
 });
 
+const swiperCareers = new Swiper('.careers-swiper__swiper', {
+
+  direction: 'horizontal',
+  modules: [Navigation],
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 500,
+  spaceBetween: 20,
+
+  wrapperClass: 'careers-swiper__wrapper',
+  slideClass: 'careers-swiper__slide',
+
+  navigation: {
+    nextEl: '.careers-swiper__next',
+    prevEl: '.careers-swiper__prev',
+  },
+});
+
 
 let header = document.querySelector('.header')
 
@@ -58,180 +74,120 @@ window.addEventListener('scroll', function () {
 
 
 let headerBurger = document.querySelector('.header__burger');
+let body = document.querySelector('body');
 
 headerBurger.addEventListener('click', function () {
   let headerMenu = document.querySelector('.header__list');
-  let body = document.querySelector('body');
 
   headerBurger.classList.toggle('burger--active');
   headerMenu.classList.toggle('header__list--active');
   body.classList.toggle('no-scrol');
 });
 
-//dropdown
+if (document.querySelectorAll('.video__video').length > 0) {
+  let videoButton = document.querySelector('.video__button');
+  let videoVideo = document.querySelector('.video__video');
+  let videoVideoClose = document.querySelector('.video__close');
 
-let headerDropdawn = document.querySelectorAll('.header__item-submenu');
+  videoButton.addEventListener('click', function () {
+    videoVideo.classList.toggle('video__video--active');
+    body.style.overflow = 'hidden';
+  });
 
-function headerDropdawnF(event) {
-  let target = event.target.closest('.header__item-submenu');
-  let targetHeight = target.clientHeight;
-  let dropdownHeader = target.querySelector('.header__submenu');
-  let dropdownHeaderHeight = dropdownHeader.clientHeight;
+  videoVideoClose.addEventListener('click', function () {
+    videoVideo.classList.remove('video__video--active');
+    videoStopper("videoContainer");
+    body.style.overflow = 'visible';
+  })
 
-  if (target.classList.contains('header__item-submenu--active')) {
-    target.classList.remove('header__item-submenu--active');
-    target.style.paddingBottom = 0;
-  }
+  videoVideo.addEventListener('click', function () {
+    videoVideo.classList.remove('video__video--active');
+    videoStopper("videoContainer");
+    body.style.overflow = 'visible';
+  })
 
-  else {
-    target.classList.add('header__item-submenu--active');
-    dropdownHeader.style.top = targetHeight + 'px';
-    target.style.paddingBottom = dropdownHeaderHeight + 'px';
-  }
-};
-
-function closeHederItem(event) {
-  let target = event.target.closest('.header__item-submenu');
-
-  if (!target) {
-    headerDropdawn.forEach(headerDropdawnItem => {
-      headerDropdawnItem.classList.remove('header__item-submenu--active');
-      headerDropdawnItem.style.paddingBottom = 0;
-    });
-
+  let videoStopper = function (id) {
+    let containerElement = document.getElementById(id);
+    let iframe_tag = containerElement.querySelector('iframe');
+    let video_tag = containerElement.querySelector('video');
+    if (iframe_tag) {
+      let iframeSrc = iframe_tag.src;
+      iframe_tag.src = iframeSrc;
+    }
+    if (video_tag) {
+      video_tag.pause();
+    }
   }
 }
+//dropd
 
-document.addEventListener('click', closeHederItem);
-
-// Проверка на мобилу
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-  headerDropdawn.forEach(headerDropdawnItem => {
-    headerDropdawnItem.addEventListener('click', headerDropdawnF);
-  });
+  let tarhetHeight = document.querySelector('.header__item-submenu').offsetHeight;
+  document.querySelector('.header__item-submenu').style.height = tarhetHeight + 'px';
+
+  function headerDropdawnF(e) {
+    let target = e.target.closest('.header__item-submenu');
+    let submenu = document.querySelector('.header__submenu');
+    let submenuHeight = submenu.offsetHeight;
+
+    if (target) {
+      target.classList.toggle('header__item-submenu--active');
+      if (target.classList.contains('header__item-submenu--active')) {
+        target.style.height = tarhetHeight + submenuHeight + 'px';
+      } else {
+        target.style.height = tarhetHeight + 'px';
+      }
+    }
+  }
+
+  document.addEventListener('click', headerDropdawnF);
 }
 
-let videoButton = document.querySelector('.video__button');
-let videoVideo = document.querySelector('.video__video');
+if (document.querySelectorAll('.plan__choice').length > 0) {
 
-videoButton.addEventListener('click', function () {
-  let body = document.querySelector('body');
+  let placeholder = document.querySelector('.plan__placeholder');
+  let choice = document.querySelector('.plan__choice');
 
-  videoVideo.classList.toggle('video__video--active');
-  body.style.overflow = 'hidden';
-});
+  placeholder.addEventListener('click', function () {
+    choice.classList.toggle('plan__choice--active');
+  });
+
+  function choiceF(e) {
+    let target = e.target.closest('.plan__duration-item');
+
+    if (target) {
+      placeholder.innerText = target.innerText;
+      choice.classList.remove('plan__choice--active');
+    }
+  }
+
+  document.addEventListener('click', choiceF);
+}
 
 
+if (document.querySelectorAll('.question').length > 0) {
+  function questionAk(e) {
+    let target = e.target.closest('.question__title');
+    let question = e.target.closest('.question');
+    let targetHeight = document.querySelector('.question__title').offsetHeight;
+    let content = e.target.closest('.question__wrapper');
 
-// Обратный отсчет
+    if (target) {
+      let qiestiomContentHeight = target.nextElementSibling.clientHeight;
+      question.classList.toggle('question--active');
 
-// let deadline = '2023-01-20';
+      question.classList.contains('question--active')
+        ? question.style.height = targetHeight + qiestiomContentHeight + 'px'
+        : question.style.height = targetHeight + 'px';
+    } else if (!target && !content) {
+      let questions = document.querySelectorAll('.question');
+      questions.forEach(item => {
+        item.classList.remove('question--active');
+        item.style.height = targetHeight + 'px';
+      })
+    }
+  }
+  document.addEventListener('click', questionAk);
+}
 
-// function getTimeRemaining(endtime) {
-//   let t = Date.parse(endtime) - Date.parse(new Date());
-//   let seconds = Math.floor((t / 1000) % 60);
-//   let minutes = Math.floor((t / 1000 / 60) % 60);
-//   let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//   let days = Math.floor(t / (1000 * 60 * 60 * 24));
-//   return {
-//     'total': t,
-//     'days': days,
-//     'hours': hours,
-//     'minutes': minutes,
-//     'seconds': seconds
-//   };
-// }
-
-// function initializeClock(id, endtime) {
-//   let clock = document.getElementById(id);
-//   let daysSpan = clock.querySelector('.days');
-//   let hoursSpan = clock.querySelector('.hours');
-//   let minutesSpan = clock.querySelector('.minutes');
-//   let secondsSpan = clock.querySelector('.seconds');
-//   function updateClock() {
-//     let t = getTimeRemaining(endtime);
-
-//     daysSpan.innerHTML = ('0' + t.days).slice(-2);
-//     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//     }
-//   }
-
-//   updateClock(); // запустите функцию один раз, чтобы избежать задержки
-//   let timeinterval = setInterval(updateClock, 1000);
-// }
-
-// initializeClock('clockdiv', deadline);
-
-// Табы
-
-// let tabButton = document.querySelectorAll('.tab__nav-item ');
-
-// function tabTab(event) {
-//   let target = event.target.closest('.tab__nav-item');
-
-//   let tabButton = document.querySelectorAll('.tab__nav-item');
-
-//   let tabTableWrapper = document.querySelectorAll('.tab__table-wrapper');
-
-//   tabButton.forEach(tabButton => {
-//     tabButton.classList.remove('tab__nav-item--active')
-//   });
-
-//   target.classList.add('tab__nav-item--active');
-
-//   for (let item of tabTableWrapper) {
-//     let slideData = item.dataset.target;
-//     let buttunData = target.dataset.target + '-tab'
-
-//     item.classList.remove('tab__table-wrapper--visible')
-
-//     if (slideData == buttunData) {
-//       item.classList.add('tab__table-wrapper--visible');
-//     }
-//   }
-
-// }
-
-// tabButton.forEach(tabItem => {
-//   tabItem.addEventListener('click', tabTab);
-// });
-
-// Анимации при скролле
-
-// const animItems = document.querySelectorAll('._anim-items');
-
-// if (animItems.length > 0) {
-//   window.addEventListener('scroll', animOnScroll);
-//   function animOnScroll() {
-//     for (let index = 0; index < animItems.length; index++) {
-//       const animItem = animItems[index];
-//       const animItemHeight = animItem.offsetHeight;
-//       const animItemOffset = offset(animItem).top;
-//       const animStart = 2;
-
-//       let animItemPoint = window.innerHeight - animItemHeight / animStart;
-//       if (animItemHeight > window.innerHeight) {
-//         animItemPoint = window.innerHeight - window.innerHeight / animStart;
-//       }
-
-//       if ((window.pageYOffset > animItemOffset - animItemPoint) && window.pageYOffset < (animItemOffset + animItemHeight)) {
-//         animItem.classList.add('_active');
-//       }
-//     }
-//   }
-//   function offset(el) {
-//     const rect = el.getBoundingClientRect(),
-//       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-//       scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-//   }
-
-//   setTimeout(() => {
-//     animOnScroll();
-//   }, 300);
-// }
